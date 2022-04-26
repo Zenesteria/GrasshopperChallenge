947cf999978ea8ccf52d025ca20c53c31acadd10
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-04-26 12:47:56 +0100

    Update 1.0

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,20 +4,20 @@ import '../styles/home.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-// import Showcase from '../Components/HomeComps/Showcase/Showcase'
-// import Stories from '../Components/HomeComps/Stories';
-// import SubShowcase from '../Components/HomeComps/SubShowcase';
-// import CodeBanner from '../Components/HomeComps/CodeBanner';
+import Showcase from '../Components/HomeComps/Showcase/Showcase'
+import Stories from '../Components/HomeComps/Stories';
+import SubShowcase from '../Components/HomeComps/SubShowcase';
+import CodeBanner from '../Components/HomeComps/CodeBanner';
 import GooglePlay from '../Components/HomeComps/GooglePlay';
 
 
 export default function Home() {
   return (
     <div className='w-full h-fit'>
-        {/* <Showcase/> */}
-        {/* <SubShowcase/> */}
-        {/* <Stories/> */}
-        {/* <CodeBanner/> */}
+        <Showcase/>
+        <SubShowcase/>
+        <Stories/>
+        <CodeBanner/>
         <GooglePlay/>
     </div>
   )
